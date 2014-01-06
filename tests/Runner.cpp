2e37acb77c898c4fe85990154045904b190536cf
Michael R. Fisher <mfisher31@gmail.com>
2014-01-06 17:49:23 -0600

    cleaning up the tests directory

--- tests/Runner.cpp
+++ tests/Runner.cpp
@@ -1,21 +1,19 @@
+/*
+    Runner.cpp - This file is part of Element
+    Copyright (C) 2013  Michael Fisher <mfisher31@gmail.com>
 
-#include "element/Core.h"
-#include "element/engine/plugin-manager.hpp"
-#include "element/engine/world.hpp"
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-using namespace Element;
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-int main()
-{
-    World* world = new World();
-    if (AudioPluginFormat* vst = world->plugins().format ("AudioUnit"))
-    {
-        StringArray plugs = vst->searchPathsForPlugins (FileSearchPath(), true);
-        for (const String& str : plugs)
-            std::cout << "plug: " << str << std::endl;
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
-        OwnedArray<PluginDescription> descs;
-        vst->findAllTypesForFile (descs, "build/Components/Element.component");
-        std::cout << "Found plugins : " << plugs.size() << std::endl;
-    }
-}
--- tests/test_Dummy.cpp
+++ tests/test_Dummy.cpp
@@ -1,7 +1,24 @@
+/*
+    test_Dummy.cpp - This file is part of Element
+    Copyright (C) 2013  Michael Fisher <mfisher31@gmail.com>
 
-#include "element/Core.h"
-using namespace Element;
-#if 1
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "Tests.h"
+
 class DummyTest : public UnitTest
 {
 public:
@@ -16,5 +33,3 @@ public:
 };
 
 static DummyTest dummy_test;
-#endif
-
