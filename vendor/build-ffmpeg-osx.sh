84d6f2c8c7b334b0af10cabeb60c25e79f13a7ca
Michael Fisher <mfisher@kushview.net>
2017-08-01 13:28:40 -0400

    Added bindir option to ffmpeg build script (osx)

--- vendor/build-ffmpeg-osx.sh
+++ vendor/build-ffmpeg-osx.sh
@@ -3,19 +3,21 @@ here=`pwd`
 top=`dirname ${here}`
 libdir="${top}/modules/kv_ffmpeg/libs/MacOSX/x86_64"
 incdir="${top}/modules/kv_ffmpeg/include"
+bindir="${top}/modules/kv_ffmpeg/bin"
 
 set -ex
 
 mkdir -p build/ffmpeg && rm -rf build/ffmpeg/*
+rm -rf "${libdir}"
+rm -rf "${incdir}"
+
 cd build/ffmpeg
 ../../ffmpeg-3.3.3/configure \
-    --disable-gpl \
-    --enable-shared \
-    --enable-static \
-    --disable-doc \
+    --disable-gpl --enable-shared --disable-static --disable-doc \
     --prefix="${here}/dist" \
     --libdir="${libdir}" --shlibdir="${libdir}" \
     --incdir="${top}/modules/kv_ffmpeg/include" \
+    --bindir="${top}/modules/kv_ffmpeg/bin" \
     && make -j8 && make install
 
 cd "${here}"
