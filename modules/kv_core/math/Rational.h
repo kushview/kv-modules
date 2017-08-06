86324beaa22e57af623981413f4a790ec18b2c78
Michael Fisher <mfisher@kushview.net>
2017-08-06 15:34:59 -0400

    Added GPL notice to rational struct as well as helper methods

--- modules/kv_core/math/Rational.h
+++ modules/kv_core/math/Rational.h
@@ -1,3 +1,24 @@
+/*
+    This file is part of the Kushview Modules for JUCE
+    Copyright (C) 2014  Kushview, LLC.  All rights reserved.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#pragma once
+
 /** Representation of a rational number */
 struct Rational
 {
@@ -19,5 +40,15 @@ struct Rational
     int den;
     
     /** Returns numerator / denominator */
-    double ratio() const { jassert(den > 0); return (double)num / (double)den; }
+    double ratio() const
+    { 
+        jassert (den > 0);
+        return (double)num / (double)den; 
+    }
+
+    /** Returns an Inverted rational */
+    Rational inverted() const           { return Rational (den, num);  }
+
+    /** Returns an inverted ratio */
+    double invertedRatio() const        { return inverted().ratio(); }
 };
