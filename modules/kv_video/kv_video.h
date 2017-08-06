9328f1a90fe676a880ffb6612426d7f011c68e47
Michael Fisher <mfisher@kushview.net>
2017-08-06 15:36:16 -0400

    Housekeeping module files

--- modules/kv_video/kv_video.h
+++ modules/kv_video/kv_video.h
@@ -1,17 +1,33 @@
 /*
-  BEGIN_JUCE_MODULE_DECLARATION
- 
-  ID:            kv_video
-  vendor:        Kushview, LLC.
-  version:       0.0.1
-  name:          Video wrapping classes for use in JUCE
-  description:   Provides classes to read media streams from video files or to
-                 mux audio into an existing video
-  dependencies:  juce_audio_basics, juce_audio_formats, kv_core
-  website:       http://kushview.net
-  license:       BSD V2 3-clause
+    This file is part of the Kushview Modules for JUCE
+    Copyright (C) 2017  Kushview, LLC.  All rights reserved.
 
-  END_JUCE_MODULE_DECLARATION
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:            kv_video
+    vendor:        Kushview
+    version:       0.0.1
+    name:          Video classes for JUCE
+    description:   Provides classes to read media streams from video sources
+    dependencies:  juce_audio_basics, juce_audio_formats, kv_core
+    website:       http://kushview.net
+    license:       GPL
+
+    END_JUCE_MODULE_DECLARATION
 */
 
 #pragma once
