de155ba8123465d5eda122845907cf83e4a5e1fe
Michael Fisher <mfisher@kushview.net>
2017-08-06 15:36:35 -0400

    Cleaning up ffmpeg module files

--- modules/kv_ffmpeg/kv_ffmpeg.h
+++ modules/kv_ffmpeg/kv_ffmpeg.h
@@ -1,19 +1,36 @@
 /*
-  BEGIN_JUCE_MODULE_DECLARATION
- 
-  ID:            kv_ffmpeg
-  vendor:        Kushview, LLC.
-  version:       0.0.1
-  name:          Provides FFmpeg library support for JUCE
-  description:   Classes used for reading/writing media formats supported by ffmpeg
-  dependencies:  juce_core, kv_video
-  website:       http://kushview.net
-  license:       GPL v3
+    This file is part of the Kushview Modules for JUCE
+    Copyright (C) 2017  Kushview, LLC.  All rights reserved.
 
-  searchpaths:   local/include
-  OSXLibs:       avcodec avformat avutil swscale
- 
-  END_JUCE_MODULE_DECLARATION
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:            kv_ffmpeg
+    vendor:        Kushview
+    version:       0.0.1
+    name:          FFmpeg library support for JUCE
+    description:   Classes used for reading/writing media streams supported by ffmpeg
+    dependencies:  juce_core, kv_video
+    website:       http://kushview.net
+    license:       GPL
+
+    searchpaths:   local/include
+    OSXLibs:       avcodec avformat avutil swscale
+
+    END_JUCE_MODULE_DECLARATION
 */
 
 #pragma once
