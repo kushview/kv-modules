594625d81808496b1967b0bfab30d2130d86c631
Michael Fisher <mfisher@kushview.net>
2017-08-20 22:22:54 -0400

    Better handling of activations with an EDD site

--- modules/kv_edd/auth/EDDOnlineUnlockStatus.h
+++ modules/kv_edd/auth/EDDOnlineUnlockStatus.h
@@ -12,6 +12,22 @@ public:
     
     virtual ~EDDOnlineUnlockStatus();
     
+    /** Returns a property from EDD extra data */
+    inline var getProperty (const Identifier& key) const { return edd [key]; }
+    
+    /** Returns true if a property exists on the edd node */
+    inline bool hasProperty (const Identifier& key) const { return edd.hasProperty (key); }
+    
+    /** Set the license key. Use this if you are using software licensing along with
+     EDD JUCE
+     */
+    inline void setLicenseKey (const String& licenseKey) {
+        edd.setProperty (licenseName, licenseKey.trim(), nullptr);
+    }
+    
+    /** Returns the currently stored license key */
+    inline String getLicenseKey() const { return edd[licenseName].toString(); }
+    
     /** Activate a license with the webserver. Note this requires you have EDD
         Software Licensing setup for your product.
      */
@@ -26,35 +42,23 @@ public:
     /** Checks the status of a license and updates the keyfile */
     UnlockResult checkLicense (const String& license);
     
-    /** Set the license key. Use this if you are using software licensing along with
-        EDD JUCE
-     */
-    void setLicenseKey (const String& licenseKey) { edd.setProperty("license", licenseKey.trim(), nullptr); }
-    
-    /** Returns the currently stored license key */
-    String getLicenseKey() const { return edd["license"].toString(); }
-    
     /** Reads the reply from your server and decrypts the edd ValueTree */
     String readReplyFromWebserver (const String& email, const String& password) override;
     
 protected:
-    /** Return the base URL to your EDD installation. Usually this will be the root
-        URL of your website 
-     */
-    virtual URL getApiEndPoint() { return URL(); }
-    
-    /** Override this to add additional query params sent to the web server */
-    virtual StringPairArray getQueryParams() { return StringPairArray(); }
-    
     /** Call this in your restoreState callback to update the edd node */
     void eddRestoreState (const String& state);
     
 private:
     /** Custom data sent back during authorization will be stored here. by default
-        this only has two properties ownedPriceIDs and downloadID. Depending on the
+        this only has two properties version and priceIDs. Depending on the
         EDD extensions you have installed, the structure of this object will change.
      */
     ValueTree edd;
+    
+    static const char* activeName;
+    static const char* enabledName;
+    static const char* licenseName;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EDDOnlineUnlockStatus);
 };
