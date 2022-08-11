28cff4ec5067b459fc05890c0d57d40f5bfbdef2
Michael Fisher <mfisher@kushview.net>
2022-08-11 18:40:39 -0400

    Slimming the codebase

--- .vscode/vscode.h
+++
@@ -1,7 +0,0 @@
-#include <juce/juce.h>
-#include <lilv/lilv.h>
-#include <suil/suil.h>
-namespace juce {}
-using namespace juce;
-
-#include "../modules/kv_lv2/kv_lv2.h"
--- extras/MoviePlayer/JuceLibraryCode/include_kv_ffmpeg.mm
+++
@@ -1,9 +0,0 @@
-/*
-
-    IMPORTANT! This file is auto-generated each time you save your
-    project - if you alter its contents, your changes may be overwritten!
-
-*/
-
-#include "AppConfig.h"
-#include <kv_ffmpeg/kv_ffmpeg.mm>
